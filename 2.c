da8e9f07f6d120bb3dc423e332905e558273f447
TerrariaIssues <crcarta@gmail.com>
2023-09-19 15:07:11 -0400

    Substitution

--- 2.c
+++ 2.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int substitute(int);
+void printbits(unsigned char);
 int main()
 {
     for(int i =0; i<16;i++)
@@ -8,9 +9,16 @@ int main()
         printf("%d , ",i);
         printf("Post Encryption:  ");
         printf("%d , ",substitute(i));
+        printbits(i);
+        printf(" --> ");
+        printbits(substitute(i));
         printf("\n");
     }
     return 0;
 }
 int substitute(int a)
-{    return ((a ^ 5) << 1); } 
+{    return ((a ^ 5) <<< 1); } 
+void printbits(unsigned char v) 
+{
+    for(int i = 7; i >= 0; i--) putchar('0' + ((v >> i) & 1));
+}
